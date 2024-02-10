9bf8eb5050ab82943ba22b3a1392c314128ff45a
gittjar <jarno.keranen@gmail.com>
2024-02-10 13:33:47 +0200

    added wakeup server script

--- blogapp-frontend/src/components/MainPage.js
+++ blogapp-frontend/src/components/MainPage.js
@@ -1,17 +1,25 @@
-// MainPage.js
-import React from 'react';
+import React, { useEffect } from 'react';
+import axios from 'axios';
 import '../css/tyylit.css'
 
 const MainPage = () => {
+  useEffect(() => {
+    const wakeUpServer = async () => {
+      try {
+        await axios.get('https://blogapp-backend-e23a.onrender.com/api/blogs');
+      } catch (err) {
+        console.error('Failed to wake up server', err);
+      }
+    };
+
+    wakeUpServer();
+  }, []);
+
   return (
     <div className="background-image-main">
       <div>Hello visitor ! <br></br>Jarno (@gittjar) created this webapp. <br></br> 
       <hr></hr>Your Blog name or Company etc.
-      
       </div>
-
-     
-      
     </div>
   );
 };
